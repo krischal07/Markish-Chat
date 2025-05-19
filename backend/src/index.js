dbe8e76319325b8e57924d6b35537b2f50a59f1f
krischal07 <krischal.shrestha9849@gmail.com>
2025-05-19 15:57:53 +0545

    Init project

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+
+
+dotenv.config()
+const app = express();
+
+const PORT = process.env.PORT
+app.use("/api/auth", authRoutes)
+app.listen(PORT, ()=> {
+    console.log("server is running on PORT:", PORT)
+})
