d6e7775202ce19d501cb460ff6878e6136377af7
krischal07 <krischal.shrestha9849@gmail.com>
2025-05-20 15:55:06 +0545

    created message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,9 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+
 app.listen(PORT, () => {
   console.log("server is running on PORT:", PORT);
   connectDb();
