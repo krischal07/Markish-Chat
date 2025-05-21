19eb129fab065bc269afa5f1db8f76e81a8db465
krischal07 <krischal.shrestha9849@gmail.com>
2025-05-21 15:56:36 +0545

    Created theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,10 @@ import Navbar from './components/Navbar'
 import { useAuthStore } from './store/useAuthStore'
 import {Loader} from "lucide-react"
 import { Toaster } from 'react-hot-toast'
-
+import { useThemeStore } from './store/useThemeStore'
 const App = () => {
 const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
-
+const{theme} = useThemeStore()
 useEffect(()=>{
   checkAuth()
 },[checkAuth])
@@ -27,7 +27,7 @@ if(isCheckingAuth && !authUser){
   )
 }
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path="/" element={authUser?<HomePage />:<Navigate to="/login"/>}/>
