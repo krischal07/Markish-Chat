66e9f2e9fb6c7b79221db8c787f901cab0ccc3c8
krischal07 <krischal.shrestha9849@gmail.com>
2025-05-24 20:22:56 +0545

    deployemnt pipeline

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,23 +4,33 @@ import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import messageRoutes from "./routes/message.route.js"
-import cors from "cors"
-import { app,server } from "./lib/socket.js";
-
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:", PORT);
